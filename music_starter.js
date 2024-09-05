007c063f2269db5dd9ffd22daff338041994982d
khloe1e <lekhue@myvuw.ac.nz>
2024-09-05 15:57:09 +1200

    Update music_starter.js
    attempt 1

--- music_starter.js
+++ music_starter.js
@@ -1,55 +1,31 @@
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(540, 960);
+  angleMode(DEGREES);
   background(0);
   strokeWeight(4);
 }
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+  background(30)
 
+  rotateX(60)
 
+  noFill()
+  stroke(255) 
 
+  for (var i = 0; i < 10; i++){
+    beginShape()
+    for (var j = 0; j < 360, j += 10); {
+      var rad = i * 8
+      var x = rad * cos(j)
+      var y = rad * sin(j)
+      var z = sin(frameCount) * 50
 
+      vertex(x, y, z)
 
+    }
 
+    endShape(CLOSE)
 
-
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+}
 }
