c25bf6eba13707cf7c2896f8e4c5ac2177ede0cf
khloe1e <lekhue@myvuw.ac.nz>
2024-09-05 16:10:55 +1200

    Update music_starter.js
    fixing problem, it kept saying I'm missing ";" on line18

--- music_starter.js
+++ music_starter.js
@@ -1,25 +1,25 @@
 function setup() {
-  createCanvas(540, 960);
+  createCanvas(540, 960, WebGL);
   angleMode(DEGREES);
   background(0);
   strokeWeight(4);
 }
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(30)
+  background(30);
 
-  rotateX(60)
+  rotateX(60);
 
-  noFill()
-  stroke(255) 
+  noFill();
+  stroke(255);
 
   for (var i = 0; i < 10; i++){
     beginShape()
-    for (var j = 0; j < 360, j += 10); {
+    for (var j = 0; j < 360, j += 10) {
       var rad = i * 8
       var x = rad * cos(j)
       var y = rad * sin(j)
-      var z = sin(frameCount) * 50
+      var z = sin(frameCount + i * 10) * 50
 
       vertex(x, y, z)
 
