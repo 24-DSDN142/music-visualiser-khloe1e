c3eac60f35f3a0aed5f900e743fc295ea6eed1c6
khloe1e <lekhue@myvuw.ac.nz>
2024-09-07 13:27:43 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -8,29 +8,22 @@ function setup() {
   angleMode(DEGREES);
   background(0);
   strokeWeight(4);
+  sound.play();
+  //create an amplitude object
+  amp = new p5.Amplitude();
+  amp.setInput(sound);
+
 }
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(30);
+  //get the sound level
+  var vol = amp.getLevel();
 
-  rotateX(60);
+  ellipse(540/2, 960/2, vol*100, vol*100);
+  ellipse(540/2 + 200, 960/2, vol*100, vol*100);
+  ellipse(540/2 - 200, 960/2, vol*100, vol*100);
 
-  noFill();
-  stroke(255);
-
-  for (var i = 0; i < 10; i++){
-    beginShape()
-    for (var j = 0; j < 360; j += 10) {
-      var rad = i * 8
-      var x = rad * cos(j)
-      var y = rad * sin(j)
-      var z = sin(frameCount + i * 10) * 50
-
-      vertex(x, y, z)
-
-    }
-
-    endShape(CLOSE)
 
 }
-}
+  
