9ee7476977ab1beadd3e75ffb17e0dca3433a540
khloe1e <lekhue@myvuw.ac.nz>
2024-09-07 14:41:02 +1200

    Update music_starter.js
    map

--- music_starter.js
+++ music_starter.js
@@ -1,5 +1,6 @@
 let wide = 960
 let high = 540
+
 function setup() {
   createCanvas(wide, high);
   angleMode(DEGREES);
@@ -15,8 +16,15 @@ function draw(words, vocal, drum, bass, other, counter) {
   var space=10
 
   for (var i=0; i<360; i += space) {
+
+    var xoff = map (cos(i), -1, 1, 0, 3)
+    var yoff = map (sin(i), -1, 1, 0, 3)
+
+    var n = noise (xoff, yoff)
+
+    var h = map (n, 0, 1, -150, 150)
     rotate(space)
-    rect(200, 0, 10)
+    rect(200, 0, h, 10)
 
   }
 }
