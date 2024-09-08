d5b9eaccaa8823b6ac4f0f8c9e4e179b57e8450a
khloe1e <lekhue@myvuw.ac.nz>
2024-09-09 01:38:29 +1200

    Update music_starter.js
    adding wobble circle as layer 2, delete fft

--- music_starter.js
+++ music_starter.js
@@ -1,14 +1,14 @@
 let wide = 540
 let high = 960
 let start = 0
-var fft
+let angle = 0
+let p = 300
 
 function setup() {
   createCanvas(wide, high);
   angleMode(DEGREES);
   rectMode(CENTER);
   background(0);
-  fft = new p5.FFT();
 
   //experimenting noiseDetail 
   //noiseDetail(lod, falloff)
@@ -16,14 +16,15 @@ function setup() {
   
 }
 
-//LAYER1 - Simple sine wave - Drum
-function draw() {
+function draw_one_frame(words, vocal, drum, bass, other,counter) {
   background(0);
   noFill();
+  let secondsCounter = floor(counter / 60);
   //stroke(255); //incase you don't want color
-  translate(wide/2, high/2);
 
-  for (var i = 0; i < 200; i++) {
+  //layer1
+  translate(wide/2, high/2);
+    for (var i = 0; i < 200; i++) {
     push()
 
     rotate (sin(frameCount + i*2) * 360)
@@ -36,36 +37,22 @@ function draw() {
 
     rect(0, 0, 200 - i * 2, 200 - i * 2, 50 -i)// for every loop, the height and width should be decreased so use the i to control
      //rect(0, 0, 200, 200, 50) last value is softened
-  }
 }
-
-
-
-
-
-
-//LAYER3 - Circular perlin noise 
-function draw(bass) {
-  background(0);
-  noStroke();
-
-  translate(wide/2, high/2);
-
-  var space = 1 //the space between rect
-
-  for (var i=0; i<360; i += space) {
-
-    var xoff = map (cos(i), -1, 1, 0, 3)
-    var yoff = map (sin(i), -1, 1, 0, 3)
-
-    var n = noise (xoff + start, yoff + start)
-
-    var h = map (n, 0, 1, -150, 150)
-    rotate(space)
-    rect(200, 0, h, 1)
-
+ //layer 2
+ angleMode(RADIANS)
+ translate(wide/2, high/2);
+  strokeWeight(16);
+  stroke(255);
+  //b = bass //originally using r but change to p because r is already used above
+  for(let i =0; i < 20; i++) {
+    p = bass
+    let angle = map(i, 0, 20, 0, PI*2)
+  let x = p * cos(angle);
+  let y = p * sin(angle);
+  point(x, y);
   }
+  angle += 0.05; //random(-0.1, 0.1);
+  p -= random(-2, 2);
 
-  start += 0.1
 }
-
+ 
